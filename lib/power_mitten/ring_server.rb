78b9b5299804e440a6acdf8d517b791cdf3c89ea
Eric Hodel <drbrain@segment7.net>
2013-03-16 22:01:47 -0700

    Show RingServer in the console

--- lib/power_mitten/ring_server.rb
+++ lib/power_mitten/ring_server.rb
@@ -5,41 +5,58 @@ class PowerMitten::RingServer < PowerMitten::Node
   config = PowerMitten::Configuration.new self
   config.maximum_workers = 1
 
+  attr_reader :expirations
+  attr_reader :registrations
+  attr_reader :service_registry
+
   def initialize options
     super
 
-    @ring_server = RingyDingy::RingServer.new Verbose: false, Dameon: false
+    @ring_server      = nil
+    @service_registry = Rinda::TupleSpace.new
 
-    @expirations   = nil
-    @registrations = nil
+    @expirations   = 0
+    @registrations = 0
+  end
+
+  def description # :nodoc:
+    service_count = @service_registry.read_all([:name, nil, nil, nil]).size
+
+    "#{service_count}\u2713 #{@registrations}\u2913 #{@expirations}\u2620 " +
+      super
   end
 
   def run
-    DRb.start_service unless DRb.primary_server
-
     notice "listening on #{DRb.uri}"
 
-    @registrations =
-      @ring_server.service_registry.notify 'write', [:name, nil, DRbObject, nil]
-
-    @expirations =
-      @ring_server.service_registry.notify 'delete', [:name, nil, DRbObject, nil]
-
     Thread.start do
-      @registrations.each do |(_, t)|
+      notifier = @service_registry.notify 'write', [:name, nil, DRbObject, nil]
+
+      notifier.each do |(_, (_, name, service, description))|
+        @registrations += 1
+
         notice "registered %p, %p at URI: %s ref: %d" %
-          [t[1], t[3], t[2].__drburi, t[2].__drbref]
+          [name, description, service.__drburi, service.__drbref]
       end
     end
 
     Thread.start do
-      @expirations.each do |(_, t)|
+      notifier = @service_registry.notify 'delete', [:name, nil, DRbObject, nil]
+
+      notifier.each do |(_, (_, name, service, description))|
+        @expirations += 1
+
         notice "expired %p, %p at URI: %s ref: %d" %
-          [t[1], t[3], t[2].__drburi, t[2].__drbref]
+          [name, description, service.__drburi, service.__drbref]
       end
     end
 
-    Rinda::RingServer.new @ring_server.service_registry
+    @ring_server = Rinda::RingServer.new @service_registry
+
+    @service_registry.write [
+      :name, 'Mitten-RingServer', DRb::DRbObject.new(self),
+      'the current ring server'
+    ]
 
     DRb.thread.join
   end
