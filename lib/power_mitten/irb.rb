3e3158827d65bf53cfe10bca6da1a78dd46117bd
Eric Hodel <drbrain@segment7.net>
2013-05-01 16:59:32 -0700

    Show some help on irb startup

--- lib/power_mitten/irb.rb
+++ lib/power_mitten/irb.rb
@@ -13,6 +13,28 @@ class PowerMitten::Irb < PowerMitten::Task
   end
 
   def run
+    puts <<-MESSAGE
+Welcome to mitten irb!
+
+You are in a PowerMitten::Irb instance.  If the control task shuts down the
+session will re-connect to the next control task that appears.
+
+You will find the following instance variables and methods useful.  You can
+find the referenced documentation using `ri`:
+
+  get_*:
+    Retrieves the given resource.  See the PowerMitten::Task for details on
+    the available resources.
+
+  @control:
+    The PowerMitten::Control task, you can ask it to create services on your
+    behalf if the get_* methods are insufficient.
+
+  @ring_lookup:
+    A RingyDingy::Lookup, you can ask it for registered services.
+
+    MESSAGE
+
     super do
       ARGV.clear
       IRB.setup nil
